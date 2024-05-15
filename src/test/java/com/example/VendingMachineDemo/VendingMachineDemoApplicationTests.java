e0e8c5c432fe33ce618ee2ef576e07c644e04f52
TRXCXS <trxcxs@163.com>
2024-05-15 22:38:17 +0800

    创建项目

---
+++ src/test/java/com/example/VendingMachineDemo/VendingMachineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VendingMachineDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendingMachineDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

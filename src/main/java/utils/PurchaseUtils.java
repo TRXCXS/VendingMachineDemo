17e118b67e81dd68da1d558e980062c7968ba83d
TRXCXS <trxcxs@163.com>
2024-05-17 02:04:11 +0800

    实现重量准确的情况下获取购买清单的方法，待测试

--- src/main/java/utils/PurchaseUtils.java
+++ src/main/java/utils/PurchaseUtils.java
@@ -23,9 +23,10 @@ public class PurchaseUtils {
      * @return 消费者购物的识别结果
      */
     public static RecognitionResult WeightIdentification(List<Layer> beforePurchase, List<Layer> afterPurchase, List<Stock> stocks, HashMap<String, Integer> goodsMap) {
-        RecognitionResult recognitionResult = new RecognitionResult();
-        HashMap<String, Integer> purchasedGoodsMap = new HashMap<>();
-        List<RecognitionException> exceptions = new ArrayList<>();
+        RecognitionResult recognitionResult = new RecognitionResult(); // 识别结果
+        HashMap<String, Integer> purchasedGoodsMap = new HashMap<>(); // 购买商品的map
+        List<RecognitionException> exceptions = new ArrayList<>(); // 识别到的异常列表
+        List<Map.Entry<String, Integer>> goodsList = SortUtils.sortGoodsMapByWeight(goodsMap); // 商品ID和商品重量组成的map，按照商品重量从高到低排序得到的列表
         SortUtils.sortLayersByIndex(beforePurchase);
         SortUtils.sortLayersByIndex(afterPurchase);
 
@@ -49,14 +50,15 @@ public class PurchaseUtils {
                     exceptions.add(exception);
                     throw exception;
                 }
-                List<HashMap<String, Integer>> purchasedGoods = getPurchasedGoodsOfLayer(stocks, i + 1, before.getWeight() - after.getWeight(), goodsMap);
-                if (purchasedGoods.size() != 1) {
+                HashMap<String, Integer> goods = getGoodsOfLayer(stocks, i + 1);
+                HashMap<String, Integer> purchasedGoods = getRecognitionItem(goods, goodsList, before.getWeight() - after.getWeight());
+                if (purchasedGoods == null) {
                     RecognitionException exception = new RecognitionException(i + 1, before.getWeight(), after.getWeight(), ExceptionEnum.UNRECOGNIZED_EXCEPTION);
                     exceptions.add(exception);
                     throw exception;
                 } else {
-                    for (String goodsId : purchasedGoods.getFirst().keySet()) {
-                        purchasedGoodsMap.put(goodsId, purchasedGoodsMap.getOrDefault(goodsId, 0) + purchasedGoods.getFirst().get(goodsId));
+                    for (String goodsId : purchasedGoods.keySet()) {
+                        purchasedGoodsMap.put(goodsId, purchasedGoodsMap.getOrDefault(goodsId, 0) + purchasedGoods.get(goodsId));
                     }
                 }
             } catch (RecognitionException ignored) {
@@ -138,19 +140,22 @@ public class PurchaseUtils {
      * @param goods            该层商品ID和商品数量组成的map
      * @param goodsList        商品ID和商品重量组成的map经过排序获得的List
      * @param weightDifference 购买重量与购买之前的重量的差值
-     * @return 一个可能的购买组合
+     * @return 一个可能的购买组合，若不存在或存在多个则返回null
      */
-    public static HashMap<String, Integer> getOneRecognitionItem(HashMap<String, Integer> goods, List<Map.Entry<String, Integer>> goodsList, int weightDifference) {
-        HashMap<String, Integer> onePurchasedGoods = new HashMap<>();
+    public static HashMap<String, Integer> getRecognitionItem(HashMap<String, Integer> goods, List<Map.Entry<String, Integer>> goodsList, int weightDifference) {
+        HashMap<String, Integer> onePurchasedGoods_forward = new HashMap<>();
+        HashMap<String, Integer> onePurchasedGoods_reverse = new HashMap<>();
 
-        if (findOneRecognitionItem(goods, goodsList, weightDifference, onePurchasedGoods, 0)) {
-            return onePurchasedGoods;
+        if (findOneRecognitionItem_forward(goods, goodsList, weightDifference, onePurchasedGoods_forward, 0) && findOneRecognitionItem_reverse(goods, goodsList, weightDifference, onePurchasedGoods_reverse, goodsList.size() - 1)) {
+            if (onePurchasedGoods_forward.equals(onePurchasedGoods_reverse)) {
+                return onePurchasedGoods_forward;
+            }
         }
         return null;
     }
 
     /**
-     * 使用回溯算法的思路获取一个可能的购买组合
+     * 使用回溯算法的思路找到一个可能的购买组合，正向寻找，即优先考虑购买重量最大的商品
      *
      * @param goods              该层商品ID和商品数量组成的map
      * @param goodsList          商品ID和商品重量组成的map经过排序获得的List
@@ -159,7 +164,7 @@ public class PurchaseUtils {
      * @param startIndex         开始考虑的商品下标
      * @return 一个可能的购买组合
      */
-    private static boolean findOneRecognitionItem(HashMap<String, Integer> goods, List<Map.Entry<String, Integer>> goodsList, int weightDifference, HashMap<String, Integer> currentCombination, int startIndex) {
+    private static boolean findOneRecognitionItem_forward(HashMap<String, Integer> goods, List<Map.Entry<String, Integer>> goodsList, int weightDifference, HashMap<String, Integer> currentCombination, int startIndex) {
         if (weightDifference == 0) {
             // 找到一个满足条件的组合
             return true;
@@ -184,7 +189,54 @@ public class PurchaseUtils {
                 }
 
                 currentCombination.put(goodsId, quantity);
-                if (findOneRecognitionItem(goods, goodsList, weightDifference - goodsWeight * quantity, currentCombination, i + 1)) {
+                if (findOneRecognitionItem_forward(goods, goodsList, weightDifference - goodsWeight * quantity, currentCombination, i + 1)) {
+                    // 从下一件商品继续考虑，如果可以找到，返回true
+                    return true;
+                } else {
+                    // 如果找不到，排除当前的商品数量组合，继续考虑
+                    currentCombination.remove(goodsId);
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 使用回溯算法的思路找到一个可能的购买组合，反向寻找，即优先考虑购买重量最小的商品
+     *
+     * @param goods              该层商品ID和商品数量组成的map
+     * @param goodsList          商品ID和商品重量组成的map经过排序获得的List
+     * @param weightDifference   购买重量与购买之前的重量的差值
+     * @param currentCombination 一个可能的购买组合
+     * @param startIndex         开始考虑的商品下标
+     * @return 一个可能的购买组合
+     */
+    private static boolean findOneRecognitionItem_reverse(HashMap<String, Integer> goods, List<Map.Entry<String, Integer>> goodsList, int weightDifference, HashMap<String, Integer> currentCombination, int startIndex) {
+        if (weightDifference == 0) {
+            // 找到一个满足条件的组合
+            return true;
+        }
+
+        for (int i = startIndex; i >= 0; i--) {
+            Map.Entry<String, Integer> goodsItem = goodsList.get(i);
+            String goodsId = goodsItem.getKey();
+            int goodsWeight = goodsItem.getValue();
+            int goodsNumber = goods.get(goodsId);
+
+            if (goodsWeight > weightDifference || goodsNumber == 0) {
+                // 跳过重量超过剩余重量差或数量为零的商品
+                continue;
+            }
+
+            // 从最大数量开始考虑
+            for (int quantity = goodsNumber; quantity >= 1; quantity--) {
+                if (goodsWeight * quantity > weightDifference) {
+                    // 当前商品重量超过剩余重量差时，终止循环
+                    continue;
+                }
+
+                currentCombination.put(goodsId, quantity);
+                if (findOneRecognitionItem_reverse(goods, goodsList, weightDifference - goodsWeight * quantity, currentCombination, i - 1)) {
                     // 从下一件商品继续考虑，如果可以找到，返回true
                     return true;
                 } else {
